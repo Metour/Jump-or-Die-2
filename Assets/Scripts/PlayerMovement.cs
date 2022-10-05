eb15c555359b3e19b425f60c3db8700c05d4344d
Metour <95858433+Metour@users.noreply.github.com>
2022-10-05 11:34:48 +0200

    Jump-or-Die-2.1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5.5f;
+    public float speed = 2f;
     private float horizontal;
+    private float vertical;
+    public float jump = 2f;
+    private Animator anim;
+    
+    public PlayableDirector director;
+
     //private Transform playerTransform;
-    private Rigidbody2D rb ;
+    private Rigidbody2D rb;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        /*if(GroundChecker.isGrounded)
+        {
+            A
+        }*/
     }
 
     // Start is called before the first frame update
@@ -25,6 +38,31 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
+        if(horizontal == 0)
+        {
+            anim.SetBool("Correr", false);
+        }
+
+        else
+        {
+            anim.SetBool("Correr", true);
+        }
+
+        //Rotarpj
+
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if(vertical == 1)
+        {
+            anim.SetBool("Saltar", true);
+        }
+
+        else
+        {
+            anim.SetBool("Saltar", false);
+        }
+
+
         //playerTransform.position += new Vector3 (horizontal * speed * Time.deltaTime, 0, 0);
         //playerTransform.position += new Vector3 (1, 0, 0)* horizontal * speed * Time.deltaTime;
         //playerTransform.Translate(Vector3.right * horizontal * speed * Time.deltaTime, Space.World);
@@ -33,8 +71,17 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2 (2f, 0f)* horizontal * speed;
+        
         //rb.AddForce(new Vector2 (100f, 0f) * horizontal * speed * Time.deltaTime); (Cosas puntuales, ejemplo: dash o salto, no para moverse)
         //rb.MovePosition(rb.position + new Vector2 (1f, 0f) * horizontal * speed * Time.deltaTime);
         //transform.position = Vector2.MoveTowards (transform.position, new Vector2 (8f, transform.position.y), horizontal * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Cinematica")
+        {
+            director.Play();
+        }        
     }
 }
